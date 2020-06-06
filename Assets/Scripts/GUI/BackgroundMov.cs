cda964527c01081534222455fd8619887fb91815
CoolLikeGoose <niken54@mail.ru>
2020-06-06 21:48:35 +0300

    Fixed background movement and Updated playerPrefabs with new script

--- Assets/Scripts/GUI/BackgroundMov.cs
+++ Assets/Scripts/GUI/BackgroundMov.cs
@@ -9,7 +9,7 @@ public class BackgroundMov : MonoBehaviour
 {
     private void Update()
     {
-        transform.Translate(new Vector2(0, -3f * Time.deltaTime * GameController.Instance.gameSpeed));
+        transform.Translate(new Vector2(0, -2.5f * Time.deltaTime * GameController.Instance.gameSpeed));
         if (transform.position.y < -5)
         {
             transform.position = new Vector2(transform.position.x, 5);
